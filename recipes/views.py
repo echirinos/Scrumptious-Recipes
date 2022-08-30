274faf4b83b2d765fb63a5096d9bc3bf78a57363
esteban <esteban.chirinos@gmail.com>
2022-08-29 21:03:16 -0400

    converted the create view for a Recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
+from django.views.generic import ListView
 
 from recipes.forms import RatingForm
 
@@ -12,20 +15,11 @@ except Exception:
     Recipe = None
 
 
-def create_recipe(request):
-    if request.method == "POST" and RecipeForm:
-        form = RecipeForm(request.POST)
-        if form.is_valid():
-            recipe = form.save()
-            return redirect("recipe_detail", pk=recipe.pk)
-    elif RecipeForm:
-        form = RecipeForm()
-    else:
-        form = None
-    context = {
-        "form": form,
-    }
-    return render(request, "recipes/new.html", context)
+class RecipeCreateView(CreateView):
+    model = Recipe
+    template_name = "recipes/new.html"
+    fields = ["name", "author", "description", "image"]
+    success_url = reverse_lazy("recipes_list")
 
 
 def change_recipe(request, pk):
@@ -49,6 +43,7 @@ def change_recipe(request, pk):
 class RecipeListView(ListView):
     model = Recipe
     template_name = "recipes/list.html"
+    paginate_by = 2
 
 
 def log_rating(request, recipe_id):
@@ -64,3 +59,8 @@ def log_rating(request, recipe_id):
 class RecipeDetailView(DetailView):
     model = Recipe
     template_name = "recipes/detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["rating_form"] = RatingForm()
+        return context
